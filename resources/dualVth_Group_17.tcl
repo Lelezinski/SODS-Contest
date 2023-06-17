0db95036806797cda97ef952aada20eeb8052352
alessandrovargiu <alessandrovargiu98@gmail.com>
2023-06-17 23:20:29 +0200

    update script

--- resources/dualVth_Group_17.tcl
+++ resources/dualVth_Group_17.tcl
@@ -1,75 +1,80 @@
-proc dualVth {slackThreshold maxFanoutEndpointCost} {
-    # Initially swap all cells to HVT
-    swap_to_hvt
-
-    while {[check_constraints $slackThreshold maxFanoutEndpointCost] == 0} {
-        # While constraints are not met
-        # Swap a random HVT cell at time to LVT
-        set hvt_cells [get_cells -filter "lib.cell.threshold_voltage_group == HVT"]
-        set random_cell [index_collection $hvt_cells]
-        set cell_name [lindex $sorted_cells 0 0]
-        swap_cell_to_lvt [get_cells $cell_name]
-    }
-    return 1
-}
-
-# Swap all cells to HVT
 proc swap_to_hvt {} {
-    foreach_in_collection cell {get_cells} {
+    foreach_in_collection cell [get_cells] {
         set ref_name [get_attribute $cell ref_name]
+
         set library_name "CORE65LPHVT"
         regsub {_LL} $ref_name "_LH" new_ref_name
-        size_cell $cell "${library_name} / ${new_ref_name}"
+        size_cell $cell "${library_name}/${new_ref_name}"
     }
 }
 
-# Swap given cell to LVT
 proc swap_cell_to_lvt {cell} {
     set ref_name [get_attribute $cell ref_name]
-    set library_name "CORE65LPHVT"
+    set library_name "CORE65LPLVT"
     regsub {_LH} $ref_name "_LL" new_ref_name
-    size_cell $cell "${library_name} / ${new_ref_name}"
+    size_cell $cell "${library_name}/${new_ref_name}"
 }
 
-# Check if constraints are met
-proc check_constraints {slackThreshold maxFanoutEndpointCost} {
+proc check_contest_constraints {slackThreshold maxFanoutEndpointCost} {
     update_timing -full
-    # TODO: Marco non è sicuro della linea dopo
-    set msc_slack {get_attribute [get_timing_paths]}
 
-    if {$msc_slack < 0}{
+    # Check Slack
+    set msc_slack [get_attribute [get_timing_paths] slack]
+
+    if {$msc_slack < 0} {
         puts "Slack: $msc_slack"
         return 0
     }
-    foreach_in_collection cell {get_cells}{
-        # TODO: Marco non sa se è completo. marco non sa molte cose
-        set path [get_timing_paths -through $cell -nworst 1 -max_paths 10000]
+
+    # Check Fanout Endpoint Cost
+    foreach_in_collection cell [get_cells] {
+        set paths [get_timing_paths -through $cell -nworst 1 -max_paths 10000 -slack_lesser_than $slackThreshold]
         set cell_fanout_endpoint_cost 0.0
         foreach_in_collection path $paths {
-            set this cost [expr $slackThreshold - [ get attribute $path slack ]]
-            # FIXME: completare linea dopo
-            set cell_fanout_endpoint_cost [expr $cell_fanout_endpoint_cost + ...]
+            set this_cost [expr $slackThreshold - [get_attribute $path slack]]
+            set cell_fanout_endpoint_cost [expr $cell_fanout_endpoint_cost + $this_cost]
         }
-        puts FCE: $cell_fanout_endpoint_cost
-        if {$cell_fanout_endpoint_cost >= $maxFanoutEndpointCost}{
-            set cell_name [get attribute $cell full_name]
+
+        if {$cell_fanout_endpoint_cost >= $maxFanoutEndpointCost} {
+            puts "FCE Violated: $cell_fanout_endpoint_cost"
+            set cell_name [get_attribute $cell full_name]
             set cell_ref_name [get_attribute $cell ref_name]
             return 0
         }
     }
+
     return 1
 }
 
-# Cell sorting algorithm: ascending order by cell slack
 proc sort_cells_by_slack {hvt_cells} {
-    # Create the list to sort
+    set sorted_cells ""
+
     foreach_in_collection cell $hvt_cells {
         set cell_path [get_timing_paths -through $cell]
         set cell_slack [get_attribute $cell_path slack]
         set cell_name [get_attribute $cell full_name]
         lappend sorted_cells "$cell_name $cell_slack"
     }
-    # Sort by ascending slack order
-    set $sorted_cells [lsort -real -increasing -index 1 $sorted_cells]
+
+    set sorted_cells [lsort -real -increasing -index 1 $sorted_cells]
+
     return $sorted_cells
+}
+
+proc dualVth {slackThreshold maxFanoutEndpointCost} {
+    # SIZE ALL TO HVT
+    swap_to_hvt
+
+    # WHILE TIMING CONSTRAINTS ARE NOT MET
+    while {[check_contest_constraints $slackThreshold $maxFanoutEndpointCost] == 0} {
+        set hvt_cells [get_cells -filter "lib_cell.threshold_voltage_group == HVT"]
+        # SORT CELLS
+        set sorted_cells [sort_cells_by_slack $hvt_cells]
+        # SIZE FIRST CELLS FROM PREVIOUS LIST TO LVT
+        set cell_name [lindex $sorted_cells 0 0]
+        # SWAP the random_cell to LVT
+        swap_cell_to_lvt [get_cells $cell_name]
+    }
+
+    return 1
 }
